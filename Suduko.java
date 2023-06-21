6d78fe35f0952de4e172cbfd59db8ad15baf1b12
shaaluk <112568360+shaaluk@users.noreply.github.com>
2023-06-21 16:48:38 +0530

    First Commit

---
+++ Suduko.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.suduko;
+
+/**
+ *
+ * @author HP
+ */
+public class Suduko {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
